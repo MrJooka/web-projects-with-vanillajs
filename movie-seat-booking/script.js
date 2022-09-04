bdd4093917c71fd22052efe5cd4647c4ec911271
Jooka <mr.jooka@gmail.com>
2022-09-04 15:26:01 +0900

    11

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -5,14 +5,45 @@ const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value;
+const updateTotal = () => {
+  total.innerText = count.innerText * ticketPrice;
+};
+
+const initialUI = () => {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    count.innerText = selectedSeats.length;
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const selectedMovie = localStorage.getItem('selectedMovie');
+  if (selectedMovie !== null) {
+    movieSelect.value = selectedMovie;
+  }
+
+  const selectedMoviePrice = localStorage.getItem('selectedMovie');
+  if (selectedMoviePrice !== null) {
+    ticketPrice = +selectedMoviePrice;
+  }
+  updateTotal();
+};
+
+initialUI();
 
 const updateSelectedCount = () => {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
   count.innerText = selectedSeats.length;
-};
 
-const updateTotal = () => {
-  total.innerText = count.innerText * ticketPrice;
+  // Copy selected seats into arr
+  // Map through array
+  // Return a new array of indexes
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 };
 
 container.addEventListener('click', (e) => {
@@ -29,5 +60,7 @@ container.addEventListener('click', (e) => {
 
 movieSelect.addEventListener('change', (e) => {
   ticketPrice = +e.target.value;
+
+  localStorage.setItem('selectedMovie', e.target.value);
   updateTotal();
 });
